2fec6eaba790fb9a066167c9982fb87b06f6afe2
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-07-15 21:05:15 +0530

    map& filter

--- Map_&_Set/map_set.js
+++ Map_&_Set/map_set.js
@@ -3,9 +3,9 @@ const myArr = [1, 2, 3, 1, 5, 2, 7, 8];
 const mySet = new Set(myArr);
 
 // again converting set to array
-const uniqueArray = [...mySet];
+/* const uniqueArray = [...mySet];
 console.log(uniqueArray);
-
+ */
 // set methods
 
 // creating set
@@ -15,4 +15,4 @@ const set = new Set([3, 4, '9', 'george', 4, 5, 3]);
 set.add(9);
 set.add({ age: 24 });
 
-console.log(set);
+// console.log(set);
